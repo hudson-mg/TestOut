5384af46646015dda275f85890bf44b6ede66d72
Mike Hudson <hudsonmg@gmail.com>
2020-08-25 21:59:53 -0600

    Project Complete

---
+++ src/main/java/Pages/IpadWiFi.java
@@ -0,0 +1,39 @@
+package Pages;
+
+import Utilities.PageUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class IpadWiFi {
+    WebDriver driver = null;
+
+    // WebElements
+    By btnCorpNet = By.xpath("//div[contains(text(), 'CorpNet')]");
+    By txtPassword = By.xpath("//input[@data-typename='PasswordBox']");
+    By btnJoin = By.id("btnJoin");
+
+    public IpadWiFi(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void selectCorpNet() {
+        System.out.println(">> Click CorpNet");
+        PageUtil.waitForVisible(driver, btnCorpNet, 3);
+        driver.findElement(btnCorpNet).click();
+    }
+
+    public void enterPassword(String password) {
+        System.out.println(">> Enter password");
+        PageUtil.waitForClickable(driver, txtPassword, 3);
+        driver.findElement(txtPassword).clear();
+        driver.findElement(txtPassword).sendKeys(password);
+    }
+
+    public void selectJoin() {
+        System.out.println(">> Click join");
+        PageUtil.waitForClickable(driver, btnJoin, 3);
+        driver.findElement(btnJoin).click();
+    }
+
+}
